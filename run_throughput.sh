86e93cdb40c1b9bba24c306765bde5a14ad4d3b6
wangfengming <wangfengming@jd.com>
2019-07-22 15:18:58 +0800

    Fixed some throughoutput bugs

--- run_throughput.sh
+++ run_throughput.sh
@@ -7,22 +7,28 @@ if [ $# -ne 9 ] && [ $# -ne 12 ]; then
 fi
 
 
-SF=1
-STREAM_COUNT
+SF=$1
+STREAM_COUNT=$2
 
 RUNTIME_DIR="./runtime"
 TEMPLATES_DIR="./templates"
 
+if [ ! -d RUNTIME_DIR]; then
+  echo "you should run power test first"
+  exit 1
+fi
+
 original_dir=`pwd`
 
 #copy query template to runtime
-for i in $seq(1, $STREAM_COUNT) {
+for i in $(seq 1 $STREAM_COUNT) 
+do
   #copy template to runtime
   cp -r  $TEMPLATES_DIR/throughput_query $RUNTIME_DIR/throughput_query_$i
   cd $RUNTIME_DIR/throughput_query_$i
   ./generate_sql.sh $SF
   cd $original_dir
-}
+done
 
 #copy update template to runtime
 cp -r  $TEMPLATES_DIR/throughput_update $RUNTIME_DIR/throughput_update
@@ -31,7 +37,8 @@ cd $RUNTIME_DIR/throughput_update
 cd $original_dir
 
 #async run query
-for i in $seq(1, $STREAM_COUNT) {
+for i in $(seq 1 $STREAM_COUNT) 
+do
   cd $RUNTIME_DIR/throughput_query_$i
   if [ $# -eq 12 ]; then
     ./run_query.sh $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 &
@@ -39,14 +46,14 @@ for i in $seq(1, $STREAM_COUNT) {
     ./run_query.sh $3 $4 $5 $6 $7 $8 $9 &
   fi
   cd $original_dir
-}
+done
 
 #async run update
 cd $RUNTIME_DIR/throughput_update
 if [ $# -eq 12 ]; then
-    ./run_update.sh $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 &
+    ./run_update.sh $3 $4 $5 $6 $7 $8 $9 $10 $11 $2 &
   else
-    ./run_update.sh $3 $4 $5 $6 $7 $8 $9 &
+    ./run_update.sh $3 $4 $5 $6 $7 $8 $2 &
   fi
 
 cd $original_dir
